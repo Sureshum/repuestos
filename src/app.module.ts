55101f99a6375ebb2dc24a3f40093468e83b4cb0
Sebastián Shum <89267654+Sureshum@users.noreply.github.com>
2025-08-27 11:53:12 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/repuestos'), 
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URL||'mongodb://localhost:27017/repuestos'), 
     RepuestosModule, UserModule, AuthModule,],
   controllers: [AppController],
   providers: [AppService],
